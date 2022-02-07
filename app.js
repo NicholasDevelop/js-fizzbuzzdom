f75ceb84acfcf95124f6c8615858ac458984d096
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-07 15:09:59 +0100

    modificato un pò il css ed aggiunte classi tramite js

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ for(i = 1; i <= 100; i++){
     console.log(i);
 
     const li = document.createElement('li');
+    li.classList.add('general-box')
     console.log(li);
     
     if((i % 3 === 0) && (i % 5 === 0) && (i !== 0)){
         li.innerHTML = 'fizzbuzz';
+        li.classList.add('fizzbuzz');
     } else if((i % 5 === 0) && (i !== 0)){
         li.innerHTML = 'buzz';
+        li.classList.add('buzz');
     } else if((i % 3 === 0) && (i !== 0)){
         li.innerHTML = 'fizz';
+        li.classList.add('fizz');
     }
+
+
 }
 
     
