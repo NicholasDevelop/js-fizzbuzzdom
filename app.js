c821cbfa76c91269d09f792cc2056ac7d97cbf29
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-07 16:02:27 +0100

    applicate modifiche alle classi di css

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const list = document.getElementById('list');
 
-let i;
-
-for(i = 1; i <= 100; i++){
+for(let i=1; i <= 100; i++){
     console.log(i);
 
     const li = document.createElement('li');
-    li.classList.add('general-box')
+    li.classList.add('general-box');
+    li.classList.add('col-1special');
+    li.innerHTML = i;
     console.log(li);
     
     if((i % 3 === 0) && (i % 5 === 0) && (i !== 0)){
@@ -19,6 +19,8 @@ for(i = 1; i <= 100; i++){
         li.innerHTML = 'fizz';
         li.classList.add('fizz');
     }
+
+    list.append(li);
 
 
 }
