fcba7e814bdc0a080da4092e124c87987b4e693d
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-07 14:34:58 +0100

    iniziata struttura base del for e dell'if

---
+++ app.js
@@ -0,0 +1,19 @@
+const list = document.getElementById('list');
+
+
+
+for(let i = 0; i < 100; i++);
+
+    const li = document.createElement('li');
+    li.append(i + 1);
+    console.log(li);
+    
+    if(i % 3 === 0){
+
+    } else if(i % 5 === 0){
+
+    } else if((i % 3 === 0) && (i % 5 === 0)){
+
+    }
+
+    
