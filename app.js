4ba15b31240254e675f72cc42f413e15e2c20107
NicholasDevelop <marcaccini.n@gmail.com>
2022-02-07 14:49:46 +0100

    conclusa prima milestone!

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const list = document.getElementById('list');
 
+let i;
 
-
-for(let i = 0; i < 100; i++);
+for(i = 1; i <= 100; i++){
+    console.log(i);
 
     const li = document.createElement('li');
-    li.append(i + 1);
     console.log(li);
     
-    if(i % 3 === 0){
-
-    } else if(i % 5 === 0){
-
-    } else if((i % 3 === 0) && (i % 5 === 0)){
-
+    if((i % 3 === 0) && (i % 5 === 0) && (i !== 0)){
+        li.innerHTML = 'fizzbuzz';
+    } else if((i % 5 === 0) && (i !== 0)){
+        li.innerHTML = 'buzz';
+    } else if((i % 3 === 0) && (i !== 0)){
+        li.innerHTML = 'fizz';
     }
+}
 
     
